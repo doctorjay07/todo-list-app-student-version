7fefed7e144f9d8c1c725faeb9036a7959932758
doctorjay07 <jayvionvicioso07@gmail.com>
2025-06-04 08:43:21 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
 // Write a comment describing the purpose of this route
 router.post("/", async (req, res) => {
   //there is a bug in line 15 you need to fix
-  const { name, description } = req.body;
+  const { title, description } = req.body;
   const task = await taskModel.addTask(title, description);
   res.status(201).json(task);
 });
